fd839f3b17c8570803cde3526af0636c3d29c70c
Ellai <ellaiyarasan@intrack.com.my>
2021-04-16 00:42:21 +0800

    feat: add write result to file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
 import fs from 'fs';
 import * as _ from 'lodash';
 import moment from 'moment';
+
+async function main() {
+    try {
+        const fileName: string = `caring${moment().format('YYYYMMDD_HHmmss')}.jsonl`;
+        console.log(fileName);
+
+        const result = [{"provider_name":"caring","url":"https://estore.caring2u.com/jom-kurang-manis/nfa-insulle-complete-nutrition-850g.html","product_id":"13801","name":"NFA INSULLE COMPLETE NUTRITION 850G","currency":"MYR","price":82.9,"promotion":false,"original_price":null,"promotion_price":null,"unit":"each","is_assorted":false}];
+
+        const data = result.map((r) => JSON.stringify(r)).join('\n');
+        fs.appendFileSync(fileName, data, 'utf8');
+
+    } catch (e) {
+        console.log('Error on Main', e);
+    }
+}
+
+if (require.main === module) {
+    main().then(() => {
+        console.log("Executed");
+    });
+}
