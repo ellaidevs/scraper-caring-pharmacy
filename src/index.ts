936fb6c4d60cf9e8dea0d492ef0329d6316eddeb
Ellai <ellaiyarasan@intrack.com.my>
2021-04-17 20:36:46 +0800

    fix: remove hardcoded result value

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,9 @@ async function main() {
             }
             result = result.concat(await scrapeMultiPage(CATEGORIES[i]));
         }
-
-        result = [{"provider_name":"caring","url":"https://estore.caring2u.com/jom-kurang-manis/nfa-insulle-complete-nutrition-850g.html","product_id":"13801","name":"NFA INSULLE COMPLETE NUTRITION 850G","currency":"MYR","price":82.9,"promotion":false,"original_price":null,"promotion_price":null,"unit":"each","is_assorted":false}];
-
+        
         const data = result.map((r) => JSON.stringify(r)).join('\n');
         fs.appendFileSync(fileName, data, 'utf8');
-
     } catch (e) {
         console.log('Error on Main', e);
     }
